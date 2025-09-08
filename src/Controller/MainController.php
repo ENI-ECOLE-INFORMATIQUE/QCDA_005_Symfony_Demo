3d18e150e12dee4d983c09c7f2bb385755335657
dsanchez <dsanchez@campus-eni.fr>
2025-09-08 14:26:38 +0200

    Module 02 - Demo02 - Modification du MainController.php. Utilisation de template twig sur le home et le test

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,11 +10,13 @@ class MainController extends  AbstractController
 {
     #[Route('/', name: 'main_home', methods: ['GET'])]
     public function home(){
-        return new Response("<html><h1>Accueil</h1></html>");
+        //return new Response("<html><h1>Accueil</h1></html>");
+        return $this->render('main/home.html.twig');
     }
 
     #[Route('/test', name: 'main_test', methods: ['GET'])]
     public function test(){
-        return new Response("<html><h1>Test</h1></html>");
+        //return new Response("<html><h1>Test</h1></html>");
+        return $this->render('main/test.html.twig');
     }
 }
