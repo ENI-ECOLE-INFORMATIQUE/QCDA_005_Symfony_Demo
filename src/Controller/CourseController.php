81c8556b3b2be3e60e6b98b7cbeac46ab959d08f
dsanchez <dsanchez@campus-eni.fr>
2025-09-09 12:10:38 +0200

    Module 04 - Demo01 - Routage et Controleurs. Création d'un controleur et routage vers le C.R.U.D

---
+++ src/Controller/CourseController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/cours', name: 'cours_')]
+final class CourseController extends AbstractController
+{
+    #[Route('/', name: 'list', methods: ['GET'])]
+    public function list(): Response
+    {
+        //TODO : Aller chercher la liste des cours dans la BD.
+        return $this->render('course/list.html.twig', [
+
+        ]);
+    }
+    #[Route('/{id}', name: 'show', requirements:['id'=>'\d+'], methods: ['GET'])]
+    public function show(int $id): Response
+    {
+        //TODO Rechercher le cours en fonction de son id dans la BD.
+        return $this->render('course/show.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/ajouter', name: 'create', methods: ['GET','POST'])]
+    public function create(): Response
+    {
+
+        return $this->render('course/create.html.twig', [
+
+        ]);
+    }
+
+    #[Route('/{id}/modifier', name: 'edit', methods: ['GET','POST'])]
+    public function edit(int $id): Response
+    {
+        //TODO Rechercher le cours en fonction de son id dans la BD.
+        return $this->render('course/edit.html.twig', [
+
+        ]);
+    }
+}
