3e72e95e0c572fecec6f77f8f735b49e9fc6ccdf
dsanchez <dsanchez@campus-eni.fr>
2025-09-10 09:30:27 +0200

    Module 05 - Demo03 - Fixtures et Faker. Mise en place des Fixtures Création de Fixtures pour l'objet Course Utilisation de Faker pour faire des fausses données

--- src/DataFixtures/CourseFixtures.php
+++ src/DataFixtures/CourseFixtures.php
@@ -10,6 +10,9 @@ class CourseFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //Permet d'utiliser Faker
+        $faker = \Faker\Factory::create('fr_FR');
+
         // $product = new Product();
         // $manager->persist($product);
         $cours= new Course();
@@ -34,12 +37,24 @@ class CourseFixtures extends Fixture
         $manager->persist($cours);
 
         //Création de 30 cours supplémentaires
-        for($i=1;$i<=30;$i++){
+        /*for($i=1;$i<=30;$i++){
             $cours= new Course();
             $cours->setName("Cours $i");
             $cours->setContent("Description du cours $i");
             $cours->setDuration(mt_rand(1,10));
             $cours->setDateCreated(new \DateTimeImmutable());
+            $manager->persist($cours);
+        }*/
+
+        for($i=1;$i<=30;$i++){
+            $cours= new Course();
+            $cours->setName($faker->word());
+            $cours->setContent($faker->realText());
+            $cours->setDuration(mt_rand(1,10));
+            $dateCreated=$faker->dateTimeBetween('-2 months', 'now');
+            $cours->setDateCreated(\DateTimeImmutable::createFromMutable( $dateCreated));
+            $dateModified=$faker->dateTimeBetween($dateCreated->format('Y-m-d'), 'now');
+            $cours->setDateModified(\DateTimeImmutable::createFromMutable( $dateModified));
             $manager->persist($cours);
         }
 
